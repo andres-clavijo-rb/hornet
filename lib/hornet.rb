3d95ed8204da5acd8a2e385e92bd51bb2bc63460
Andres Clavijo <aclavijo@abatechnology.com>
2023-10-10 20:26:08 -0500

    initial commit

---
+++ lib/hornet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hornet/version"
+
+module Hornet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hornet_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hornet do
+  it "has a version number" do
+    expect(Hornet::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
